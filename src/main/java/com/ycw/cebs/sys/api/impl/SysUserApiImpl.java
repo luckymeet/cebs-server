ba041926fd3728f7a94a5abc751f230e0b0c2d98
yuminjun <yuminjun@lexiangbao.com>
2020-05-28 17:59:44 +0800

    用户管理 身份证正则校验 修改

--- src/main/java/com/ycw/cebs/sys/api/impl/SysUserApiImpl.java
+++ src/main/java/com/ycw/cebs/sys/api/impl/SysUserApiImpl.java
@@ -64,7 +64,7 @@ public class SysUserApiImpl implements ISysUserApi {
 	@Autowired
 	private static final String DEFAULT_PASSWORD = "123456";
 
-	private static final String ID_CARD_REGEX = "^[1-9]\\d{5}(18|19|([23]\\d))\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$";
+	private static final String ID_CARD_REGEX = "^[1-9]\\d{5}(19|20)\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$";
 
 	/**
 	 * 用户列表分页查询
