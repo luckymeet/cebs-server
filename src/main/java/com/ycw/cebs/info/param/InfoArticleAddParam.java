2e09692f2db06cbf60117d7280e649466523746e
yuminjun <yuminjun@yuminjun>
2020-06-06 15:35:20 +0800

    文章新增、修改参数校验

--- src/main/java/com/ycw/cebs/info/param/InfoArticleAddParam.java
+++ src/main/java/com/ycw/cebs/info/param/InfoArticleAddParam.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.ycw.common.constants.CommonConstants;
@@ -21,6 +22,7 @@ public class InfoArticleAddParam implements Serializable {
 	 * 标题
 	 */
 	@NotBlank(message = "请输入标题")
+	@Length(max = 50, message = "标题长度不能超过50个字符")
 	private String title;
 
 	/**
@@ -41,6 +43,7 @@ public class InfoArticleAddParam implements Serializable {
 	/**
 	 * 原文链接
 	 */
+	@Length(max = 500, message = "源链接不能超过500个字符")
 	private String sourceLink;
 
 	/**
@@ -51,24 +54,22 @@ public class InfoArticleAddParam implements Serializable {
 	/**
 	 * 简介
 	 */
+	@Length(max = 200, message = "简介长度不能超过200个字符")
 	private String introduction;
 
 	/**
 	 * 内容
 	 */
-	@NotBlank(message = "请输入内容")
 	private String content;
 
 	/**
 	 * 是否推荐：0-否，1-是
 	 */
-	@NotNull(message = "请选择是否推荐")
 	private Integer isRecommend;
 
 	/**
 	 * 状态：1-生效，0-未生效
 	 */
-	@NotNull(message = "请选择生效状态")
 	private Integer status;
 
 	/**
@@ -80,7 +81,6 @@ public class InfoArticleAddParam implements Serializable {
 	/**
 	 * 是否展示免责申明：1-展示，0-不展示
 	 */
-	@NotNull(message = "请选择是否展示免责声明")
 	private Integer isShowDisclaimer;
 
 }
