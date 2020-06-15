f7ee8d68b9489567b46122c3308bcbba2b1652d4
yuminjun <yuminjun@lexiangbao.com>
2020-06-15 16:25:15 +0800

    session 改为 redis 管理

---
+++ src/main/java/com/ycw/cebs/common/config/RedisManagerProperties.java
@@ -0,0 +1,20 @@
+package com.ycw.cebs.common.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedisManagerProperties {
+
+	private String host = "localhost";
+	private String password;
+	private int port = 6379;
+	private int database = 0;
+
+}
